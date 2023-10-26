a665de075de8e9a024e080da7ebb3a14788ef071
drumforgod82 <jdrebel2@hotmail.com>
2023-10-26 16:52:02 -0500

    edited index.html

---
+++ backend/tests/TestCounter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Diagnostics.Contracts;
+using Xunit.Sdk;
+
+namespace tests
+[
+
+    public class TestCounter
+    {
+        private readonly ILogger Logger = TestFactory.CreateLogger();
+
+        [Fact]
+
+        public async void Http_trigger_should_return_known_string()
+        {
+            // Arrange
+            var counter = new Company.Function.Counter();
+            counter.Id = "1";
+            counter.Count = 2;
+            var request = TestFactory.CreateHttpRequest();
+
+            // Act
+            var response = (HttpResponseMessage) Company.Function.GetResumeCounter.Run(request, counter, out counter, logger);
+
+            // Assert
+            Assert.Equal(3, counter.Count);
+        }
+    }
+]
