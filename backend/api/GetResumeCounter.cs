67b6eaa69f207811594c1b4e7e43e4ae49115351
drumforgod82 <jdrebel2@hotmail.com>
2023-10-27 09:07:13 -0500

    Update GetResumeCounter.cs

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -12,9 +12,9 @@ using System.Text;
 
 namespace Company.Function
 {
-    public static class GetResumeCounter
+    public static class GetResumeCounterJamesDean
     {
-        [FunctionName("GetResumeCounter")]
+        [FunctionName("GetResumeCounterJamesDean")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
