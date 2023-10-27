136b888dace5617df5131fb5fa3cfb8c2454d3e8
drumforgod82 <jdrebel2@hotmail.com>
2023-10-27 08:59:44 -0500

    Update CounterOutputType.cs

--- backend/api-net6/CounterOutputType.cs
+++ backend/api-net6/CounterOutputType.cs
@@ -4,7 +4,7 @@ using Microsoft.Azure.Functions.Worker.Http;
 namespace Company.Function;
 public class MyOutputType
 {
-    [CosmosDBOutput(databaseName: "CloudResume", collectionName: "Counter", ConnectionStringSetting = "CosmosDbConnectionString")]
+    [CosmosDBOutput(databaseName: "AzureResume", collectionName: "Counter", ConnectionStringSetting = "CosmosDbConnectionString")]
     public Counter? UpdatedCounter { get; set; }
     public HttpResponseData? HttpResponse { get; set; }
 }
